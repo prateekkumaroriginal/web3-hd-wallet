c0e7895d882b6d84b95bebd889f30a38f7c724cc
Prateek Kumar <prateek.kumar.original@gmail.com>
2024-08-20 18:18:17 +0530

    Send Transaction and get balance

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,33 @@
-import { ChevronDown, ChevronRight, Copy, Eye } from 'lucide-react';
+import { ChevronDown, ChevronRight, Copy, Eye, RefreshCcw, Send } from 'lucide-react';
 import { useState } from 'react';
 import './App.css';
-import { generateMnemonic } from 'bip39';
+import { generateMnemonic, mnemonicToSeedSync } from 'bip39';
 import { Buffer } from "buffer";
-import axios from 'axios';
-import { VITE_APP_BACKEND_URL } from './lib/constants';
+import { derivePath } from 'ed25519-hd-key';
+import nacl from 'tweetnacl';
+import { Keypair, sendAndConfirmTransaction, PublicKey, Transaction, clusterApiUrl, Connection, SystemProgram, LAMPORTS_PER_SOL } from '@solana/web3.js';
 
 window.Buffer = Buffer
 
 interface wallet {
-  coinType: string;
   address: string;
   privateKey: string;
 }
 
 function App() {
-  const [coinType, setCoinType] = useState<string>("bitcoin");
   const [mnemonicWords, setMnemonicWords] = useState<string[]>([]);
   const [showing, setShowing] = useState(false);
   const [wallets, setWallets] = useState<wallet[]>([]);
   const [showAddresses, setShowAddresses] = useState<boolean[]>([]);
   const [showPrivateKeys, setShowPrivateKeys] = useState<boolean[]>([]);
+  const [toAddress, setToAddress] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [selectedWallet, setSelectedWallet] = useState({
+    position: "",
+    balance: "",
+    fromAddress: "",
+    fromHexPrivateKey: ""
+  });
 
   const genNewMnemonic = () => {
     const words = generateMnemonic(128);
@@ -37,20 +44,66 @@ function App() {
     setShowing(true);
   }
 
+  const getSolanaAddressAndPrivateKey = (mnemonic: string) => {
+    const seed = mnemonicToSeedSync(mnemonic);
+    const path = `m/44'/501'/${wallets.length}'/0'`;
+    const derivedSeed = derivePath(path, seed.toString("hex")).key;
+    const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
+    const keypair = Keypair.fromSecretKey(secret);
+
+    return {
+      address: keypair.publicKey.toBase58(),
+      privateKey: Buffer.from(keypair.secretKey).toString('hex')
+    }
+  }
+
+  const getCurrentBalance = async (address: string) => {
+    if (address.length === 0) {
+      return alert("Please enter address");
+    }
+
+    let connection = new Connection(clusterApiUrl("mainnet-beta"));
+    const fromPubkey = new PublicKey(address);
+    const currentBalance = await connection.getBalance(fromPubkey) / LAMPORTS_PER_SOL;
+
+    console.log(currentBalance, 'SOL');
+    setSelectedWallet(selectedWallet => ({ ...selectedWallet, balance: `${currentBalance}` }))
+  }
+
+  const sendTransaction = async (fromHexPrivateKey: string, toAddress: string, amount: number) => {
+    try {
+      let connection = new Connection(clusterApiUrl("mainnet-beta"));
+      let transaction = new Transaction();
+
+      const fromSecretKey = Uint8Array.from(Buffer.from(fromHexPrivateKey, 'hex'));
+      const fromKeyPair = Keypair.fromSecretKey(fromSecretKey);
+      const fromAddress = fromKeyPair.publicKey.toBase58();
+      const toPubkey = new PublicKey(toAddress);
+
+      transaction.add(
+        SystemProgram.transfer({
+          fromPubkey: fromKeyPair.publicKey,
+          toPubkey,
+          lamports: amount * LAMPORTS_PER_SOL,
+        })
+      );
+
+      await sendAndConfirmTransaction(connection, transaction, [fromKeyPair]);
+      getCurrentBalance(fromAddress);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   const generateWallet = async () => {
     try {
-      if (!mnemonicWords || !coinType) {
+      if (!mnemonicWords.length) {
         return;
       }
 
-      const response = await axios.post(`${VITE_APP_BACKEND_URL}/api/generateWallet`, {
-        mnemonic: mnemonicWords.join(" "),
-        coinType,
-        index: wallets.length
-      });
-      const data = response.data;
+      const data = getSolanaAddressAndPrivateKey(mnemonicWords.join(" "));
 
-      setWallets(wallets => [...wallets, { ...data, coinType }])
+      setWallets(wallets => [...wallets, data])
       setShowAddresses(showAddresses => [...showAddresses, true]);
       setShowPrivateKeys(showPrivateKeys => [...showPrivateKeys, false]);
     } catch (error) {
@@ -68,21 +121,30 @@ function App() {
 
   return (
     <>
-      <div className='flex p-6 px-10'>
-        <div className='flex flex-col gap-y-4 items-center w-full p-20 bg-slate-700'>
-          <button
-            onClick={handleClick}
-            className='w-full py-2 px-4 bg-purple-700 text-white font-semibold hover:opacity-80 transition'
-          >
-            Generate Mnemonic
-          </button>
+      <div className='grid grid-cols-3 p-6 px-10 gap-4 min-h-screen'>
+        <div className='col-span-2 flex flex-col gap-y-4 items-center w-full py-4 px-20 border-slate-100/20 border'>
+          <div className="flex w-full gap-4">
+            <button
+              onClick={handleClick}
+              className='w-1/2 py-2 px-4 bg-slate-900/40 hover:bg-indigo-500 border-slate-100/20 hover:border-transparent border-2 rounded text-white font-semibold transition'
+            >
+              Generate Mnemonic
+            </button>
+
+            <button
+              onClick={generateWallet}
+              className='w-1/2 py-2 px-4 bg-slate-900/40 hover:bg-indigo-500 border-slate-100/20 hover:border-transparent border-2 rounded text-white font-semibold transition'
+            >
+              Generate Wallet
+            </button>
+          </div>
 
           {mnemonicWords.length > 0 && (
             <>
               <div className="flex w-full gap-4">
-                <div
+                <button
                   onClick={() => setShowing(showing => !showing)}
-                  className='flex justify-center cursor-pointer font-semibold text-white w-full h-full p-2 bg-slate-500 hover:opacity-80'
+                  className='flex justify-center w-1/2 py-2 px-4 bg-slate-900/20 hover:bg-indigo-500 border-slate-100/20 hover:border-transparent border-2 rounded text-white font-semibold transition'
                 >
                   {showing ? (<>
                     Hide
@@ -92,15 +154,16 @@ function App() {
                     Show
                     <ChevronRight className='ml-4 w-6 h-6' />
                   </>)}
-                </div>
-                <div
+                </button>
+                <button
                   onClick={copyToClipboard}
-                  className='flex justify-center cursor-pointer font-semibold text-white w-full h-full p-2 bg-slate-500 hover:opacity-80'
+                  className='flex justify-center w-1/2 py-2 px-4 bg-slate-900/20 hover:bg-indigo-500 border-slate-100/20 hover:border-transparent border-2 rounded text-white font-semibold transition'
                 >
                   Copy
                   <Copy className='ml-4 w-6 h-6' />
-                </div>
+                </button>
               </div>
+
               {showing && <div className='flex flex-row bg-slate-900/50 p-2 justify-center w-full'>
                 <div className='flex flex-wrap w-full justify-center gap-2 p-4'>
                   {mnemonicWords.map(word => (
@@ -113,40 +176,15 @@ function App() {
             </>
           )}
 
-          <div className="flex w-full gap-4">
-            <label className='flex text-2xl w-[300px] font-semibold text-slate-400' htmlFor="blockchain">Choose blockchain:</label>
-            <div className='flex w-full gap-4'>
-              <select
-                id='blockchain'
-                className='w-full'
-                defaultValue="0"
-                onChange={(e) => setCoinType(e.target.value)}>
-                <option value="bitcoin">Bitcoin</option>
-                <option value="ethereum">Ethereum</option>
-                <option value="solana">Solana</option>
-              </select>
-              <button
-                onClick={generateWallet}
-                className='w-full py-2 px-4 bg-purple-700 text-white font-semibold hover:opacity-80 transition'
-              >
-                Generate Wallet
-              </button>
-            </div>
-          </div>
-
           {wallets.length > 0 && (
             <div className='flex flex-col w-full gap-y-4'>
-              <div className='mt-10 text-white text-center text-2xl font-semibold'>
+              <div className='mt-4 text-white text-center text-3xl font-semibold'>
                 Wallets
               </div>
 
               <div className='flex flex-col gap-y-2 bg-slate-900/50 p-2 justify-center w-full'>
                 {wallets.map((wallet, index) => (
-                  <div className='flex w-full items-center justify-between gap-2 p-2 bg-slate-700 text-zinc-400'>
-                    <div className='font-semibold uppercase mr-2'>
-                      {wallet.coinType}
-                    </div>
-
+                  <div className='flex w-full items-center justify-between gap-2 p-2 bg-slate-700 text-zinc-400 h-fit'>
                     <div className='flex flex-col gap-y-1 ml-auto w-full'>
                       <div className='flex items-center justify-between w-full gap-x-2'>
                         <span>Address: </span>
@@ -156,17 +194,17 @@ function App() {
                           type={showAddresses[index] ? "text" : "password"}
                           value={wallet.address}
                         />
-                        <div className='p-1 hover:bg-slate-400/30 hover:text-white cursor-pointer transition'>
-                          <Eye
-                            onClick={() => toggleVisibility(index, true)}
-                            className='w-4 h-4'
-                          />
+                        <div
+                          onClick={() => toggleVisibility(index, true)}
+                          className='p-1 rounded hover:bg-slate-400/30 hover:text-white cursor-pointer transition'
+                        >
+                          <Eye className='w-4 h-4' />
                         </div>
-                        <div className='p-1 hover:bg-slate-400/30 hover:text-white cursor-pointer transition'>
-                          <Copy
-                            onClick={() => navigator.clipboard.writeText(wallet.address)}
-                            className='w-4 h-4'
-                          />
+                        <div
+                          onClick={() => navigator.clipboard.writeText(wallet.address)}
+                          className='p-1 rounded hover:bg-slate-400/30 hover:text-white cursor-pointer transition'
+                        >
+                          <Copy className='w-4 h-4' />
                         </div>
                       </div>
                       <div className='flex items-center justify-between w-full gap-x-2'>
@@ -177,13 +215,13 @@ function App() {
                           type={showPrivateKeys[index] ? "text" : "password"}
                           value={wallet.privateKey}
                         />
-                        <div className='p-1 hover:bg-slate-400/30 hover:text-white cursor-pointer transition'>
+                        <div className='p-1 rounded hover:bg-slate-400/30 hover:text-white cursor-pointer transition'>
                           <Eye
                             onClick={() => toggleVisibility(index, false)}
                             className='w-4 h-4'
                           />
                         </div>
-                        <div className='p-1 hover:bg-slate-400/30 hover:text-white cursor-pointer transition'>
+                        <div className='p-1 rounded hover:bg-slate-400/30 hover:text-white cursor-pointer transition'>
                           <Copy
                             onClick={() => navigator.clipboard.writeText(wallet.privateKey)}
                             className='w-4 h-4'
@@ -195,8 +233,84 @@ function App() {
                 ))}
               </div>
             </div>
-
           )}
+        </div>
+
+        <div className='max-h-screen pb-10 sticky top-6'>
+          <div className='col-span-1 flex flex-col h-full py-4 border-slate-100/20 border'>
+            <div className='text-white text-center text-3xl font-semibold'>
+              {parseInt(selectedWallet.position) === -1 || !selectedWallet.fromAddress ? "Unknown Wallet" : `Account ${selectedWallet.position}`}
+            </div>
+
+            <div className='flex text-xs justify-center items-center text-zinc-300'>
+              <div
+                onClick={() => navigator.clipboard.writeText(selectedWallet.fromAddress)}
+                className='flex justify-center items-center px-1 cursor-pointer'
+              >
+                {selectedWallet.fromAddress && <>
+                  {selectedWallet.fromAddress.slice(0, 3)} ... {selectedWallet.fromAddress.slice(-3)}
+                  <Copy
+                    className='ml-2 w-3 h-3'
+                  />
+                </>}
+              </div>
+            </div>
+
+            <div className='mt-8 flex justify-center items-center text-2xl text-center py-4 text-white'>
+              {selectedWallet.balance} SOL
+              <span
+                onClick={() => {
+                  const fromSecretKey = Uint8Array.from(Buffer.from(selectedWallet.fromHexPrivateKey, 'hex'));
+                  const fromKeyPair = Keypair.fromSecretKey(fromSecretKey);
+                  const fromAddress = fromKeyPair.publicKey.toBase58();
+                  getCurrentBalance(fromAddress)
+                }}
+                className='ml-2 p-1 text-zinc-300 cursor-pointer'
+              >
+                <RefreshCcw className='h-3 w-3' />
+              </span>
+            </div>
+
+            <div className="flex flex-col mt-8 px-10 gap-8">
+              <div className='flex justify-center gap-4'>
+                <div className='flex flex-col justify-center w-20'>
+                  <label className='flex justify-center text-2xl font-semibold text-slate-400' htmlFor="amount">Send</label>
+                  <span className='-mt-1 text-center text-xs text-zinc-500'>Amount</span>
+                </div>
+                <input id='amount' className='p-1 h-fit w-full bg-transparent text-white border-slate-100/20 border-2 rounded outline-none focus:ring-2 focus:ring-slate-200/60' type="number" value={amount} onChange={e => setAmount(parseFloat(e.target.value))} />
+              </div>
+
+              <div className='flex justify-center gap-4'>
+                <div className='flex flex-col justify-center w-20'>
+                  <label className='flex justify-center text-2xl font-semibold text-slate-400' htmlFor="senderPrivateKey">From</label>
+                  <span className='-mt-1 text-center text-xs text-zinc-500'>Sender Private Key (Hex)</span>
+                </div>
+                <input autoComplete='off' id='senderPrivateKey' className='p-1 h-fit w-full bg-transparent text-white border-slate-100/20 border-2 rounded outline-none focus:ring-2 focus:ring-slate-200/60' type="text" value={selectedWallet.fromHexPrivateKey} onChange={e => setSelectedWallet(selectedWallet => ({
+                  ...selectedWallet,
+                  fromHexPrivateKey: e.target.value,
+                }))} />
+              </div>
+
+              <div className='flex justify-center gap-4'>
+                <div className='flex flex-col justify-center w-20'>
+                  <label className='flex justify-center text-2xl font-semibold text-slate-400' htmlFor="to">To</label>
+                  <span className='-mt-1 text-center text-xs text-zinc-500'>Receiver Address</span>
+                </div>
+                <input autoComplete='off' id='to' className='p-1 h-fit w-full bg-transparent text-white border-slate-100/20 border-2 rounded outline-none focus:ring-2 focus:ring-slate-200/60' type="text" value={toAddress} onChange={e => setToAddress(e.target.value)} />
+              </div>
+            </div>
+
+            {selectedWallet.balance && <div className="flex justify-center mt-8">
+              <button
+                onClick={() => {
+                  sendTransaction(selectedWallet.fromHexPrivateKey, toAddress, amount)
+                }}
+                className='flex py-2 px-4 bg-indigo-500 hover:bg-indigo-500/80 rounded text-white font-semibold transition'
+              >
+                Send <Send className='ml-2 h-6 w-6' />
+              </button>
+            </div>}
+          </div>
         </div>
       </div>
     </>
